83a1cc78c6a24a62bbf30ba8318f397b253367b0
llpingll <94099281+llpingll@users.noreply.github.com>
2023-10-31 17:24:57 +0000

    Add first ship test | Refactor ship to suit test

--- src/factories/tests/ship.test.js
+++ src/factories/tests/ship.test.js
@@ -2,5 +2,11 @@ import Ship from '../ship'
 
 // Describe creates a block that groups together several related tests.
 describe('Ship', () => {
+
+    const ship = Ship(3)
     
+    test('creates and initializes a ship', () => {
+        expect(ship).toEqual({ length: 3, hits: 0 });
+    });
+
 })
