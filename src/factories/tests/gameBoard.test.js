80476a47f059edc7fcdcba24225cb6d4e00077c8
llpingll <94099281+llpingll@users.noreply.github.com>
2023-11-09 15:57:03 +0000

    Add tests for isPlacementPossible function

--- src/factories/tests/gameBoard.test.js
+++ src/factories/tests/gameBoard.test.js
@@ -10,7 +10,7 @@ describe("Gameboard", () => {
     let testMissedShots;
 
     beforeEach(() => {
-        // Create new instances of all test objects
+        // Create new instances of test objects
         ship = Ship(3);
         gameboard = Gameboard();
         testBoard = [];
@@ -37,5 +37,25 @@ describe("Gameboard", () => {
         testBoard[2][1] = ship;
         testBoard[3][1] = ship;
         expect(gameboard.getBoard()).toEqual(testBoard);
+    });
+
+    test("Prevents ship placement outside gameboard", () => {
+        gameboard.placeShip(ship, 1, 1, true);
+        expect(gameboard.isPlacementPossible(ship, 8, 8, true)).toBe(false);
+        expect(gameboard.isPlacementPossible(ship, 10, 10, true)).toBe(false);
+    });
+
+    test("Prevents ship placement on taken fields", () => {
+        gameboard.placeShip(ship, 1, 1, true);
+        expect(gameboard.isPlacementPossible(ship, 1, 1, true)).toBe(false);
+    });
+    
+    test("Prevents ship placement on direct neighbouring taken fields", () => {
+        gameboard.placeShip(ship, 2, 2, true);
+        expect(gameboard.isPlacementPossible(ship, 1, 1, true)).toBe(false);        
+        expect(gameboard.isPlacementPossible(ship, 1, 1, false)).toBe(false);
+        expect(gameboard.isPlacementPossible(ship, 4, 3, true)).toBe(false);
+        expect(gameboard.isPlacementPossible(ship, 4, 3, false)).toBe(false);
+        expect(gameboard.isPlacementPossible(ship, 1, 4, true)).toBe(true);
     });
 });
