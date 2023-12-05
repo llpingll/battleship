792fb143aafe811b18a0ed4e56635a90a00251de
llpingll <94099281+llpingll@users.noreply.github.com>
2023-12-05 20:43:18 +0000

    Update packages

--- src/dom.js
+++ src/dom.js
@@ -1,22 +1,23 @@
-import { game } from "./game"
+import { playRound } from './game';
 
 const dom = (() => {
     // Cache dom gameboards
     const playerGrid = document.getElementById('playerGrid');
     const computerGrid = document.getElementById('computerGrid');
-
-    computerGrid.addEventListener("click", game.playRound(e));
-
+    console.log("hello");
     // Example of creating grid programmatically
-    // function initializeGrid() {
+    // function renderGameboard() {
     //     for (let row = 0; row < numRows; row++) {
-    //       for (let column = 0; column < numColumns; column++) {
-    //         const cell = document.createElement('div');
-    //         cell.classList.add('cell');
-    //         cell.setAttribute('data-row', row);
-    //         cell.setAttribute('data-column', column);
-    //         grid.appendChild(cell);
-    //       }
+    //         for (let column = 0; column < numColumns; column++) {
+    //             const cell = document.createElement('div');
+    //             cell.classList.add('cell');
+    //             cell.setAttribute('data-row', row);
+    //             cell.setAttribute('data-column', column);
+    //             grid.appendChild(cell);
+    //          }
     //     }
-    //   }
+    // }
+
+    computerGrid.addEventListener("click", playRound);
+
 })();
