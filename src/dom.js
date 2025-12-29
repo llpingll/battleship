6dfc6e245a33df3ff8c9a885d8e88c9cbdcd0063
llpingll <94099281+llpingll@users.noreply.github.com>
2025-12-29 19:22:15 +0000

    Update hover length to be dynamic

--- src/dom.js
+++ src/dom.js
@@ -45,17 +45,21 @@ const dom = (() => {
     }
 
     const updateHoverLength = (shipLength, isVertical) => {
-        const cellHovers = document.querySelectorAll(".cell-hover");
-        cellHovers.forEach(cellHover => {
-            cellHover.style.setProperty("--width","100%");
-            cellHover.style.setProperty("--height","100%");
-            if (!isVertical) {
-                cellHover.style.setProperty("--width",`calc(37.167px + 39.834px * ${shipLength - 1})`);
-            } else {
-                cellHover.style.setProperty("--height",`calc(37.167px + 39.834px * ${shipLength - 1})`);
-            }
-        });
-    }
+    const placementBoard = document.getElementById("placement-board");
+    const cellWidth = (placementBoard.clientWidth - 8) / 10;
+    const cellHeight = (placementBoard.clientHeight - 8) / 10;
+    
+    const cellHovers = document.querySelectorAll(".cell-hover");
+    cellHovers.forEach(cellHover => {
+        cellHover.style.setProperty("--width","100%");
+        cellHover.style.setProperty("--height","100%");
+        if (!isVertical) {
+            cellHover.style.setProperty("--width",`calc(${cellWidth}px + ${cellWidth}px * ${shipLength - 1})`);
+        } else {
+            cellHover.style.setProperty("--height",`calc(${cellHeight}px + ${cellHeight}px * ${shipLength - 1})`);
+        }
+    });
+}
 
     // Gameboards
     const renderGameboard = (gameboard, owner, enemy) => {
